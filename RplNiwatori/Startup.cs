cb05e4ee49cad10e42e5d518fc8ba3cc4cb39498
ernandarn <ernandarn@gmail.com>
2016-12-09 10:38:24 +0700

    Add project files.

---
+++ RplNiwatori/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RplNiwatori.Startup))]
+namespace RplNiwatori
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
